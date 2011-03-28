aa92e1afffe5ad97d8c6bd82fecabe57de4baecd
Eudis Duran <eudisd@live.com>
2011-03-27 21:35:08 -0400

    Removed Pylons setup.

--- save-game/savegame/tests/functional/test_main.py
+++
@@ -1,7 +0,0 @@
-from savegame.tests import *
-
-class TestMainController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='main', action='index'))
-        # Test response...
