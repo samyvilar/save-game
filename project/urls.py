465a59bc3c0c68582011ea88ab1b75d8859f907b
Eudis Duran <eudisd@live.com>
2011-04-06 18:07:23 -0400

    Fixing static files for development.  Thing is tricky.

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,9 +12,9 @@ urlpatterns = patterns('',
     #  url(r'/account/settings', 'project.views.settings', name='settings')
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'settings/', 'savegame.views.settings')
+    url(r'settings/', 'savegame.views.settings'),
     # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 )
 
 urlpatterns += staticfiles_urlpatterns()
