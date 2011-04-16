a252ad8daeeffb4d065ad977d351d9290c205585
Townes Wang <townes.wang@hotmail.com>
2011-04-16 14:51:53 -0400

    Changed urls.py to allow for the redirection of both "/signIn" and "/signin". Same for "/signOut" and "/signout".

--- project/urls.py
+++ project/urls.py
@@ -3,25 +3,25 @@ from django.conf.urls.defaults import patterns, include, url
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'savegame.views.mainpage'),
-    url(r'^registration/$', 'savegame.views.regpage'), 
-    url(r'^registration/thanks/$', 'savegame.views.regthanks'), 
-    
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    url(r'settings/', 'savegame.views.settings'),
+                       url(r'^$', 'savegame.views.mainpage'),
+                       url(r'^registration/$', 'savegame.views.regpage'),
+                       url(r'^registration/thanks/$', 'savegame.views.regthanks'),
 
-    url(r'signIn/', 'savegame.views.signIn'),
-    url(r'signOut/', 'savegame.views.signOut'),
-    url(r'gamepage/', 'savegame.views.gamepage'),
-    url(r'results/', 'savegame.views.results'),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls))
+                       url(r'settings/', 'savegame.views.settings'),
+                       url(r'sign[Ii]n/', 'savegame.views.signIn'),
+                       url(r'sign[Oo]ut/', 'savegame.views.signOut'),
+                       url(r'gamepage/', 'savegame.views.gamepage'),
+                       url(r'results/', 'savegame.views.results'),
+
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls))
 )
 
 #urlpatterns += staticfiles_urlpatterns()
