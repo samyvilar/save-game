5c371dce9854ff78e5d30db413fc1d5b70b6168e
Townes Wang <townes.wang@hotmail.com>
2011-04-27 23:31:36 -0400

    Updated the README file. The URLs in the second block of README file may need to be tweaked to work with the saved games uploaded by the users.

--- project/urls.py
+++ project/urls.py
@@ -28,8 +28,6 @@ urlpatterns = patterns('',
                        # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
-
-
-)
+                       )
 
 #urlpatterns += staticfiles_urlpatterns()
