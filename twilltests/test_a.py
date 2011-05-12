923915426bf4a3f8429306fd5bbc6604a3157279
Akshai Sarma <akshai.sarma@macaulay.cuny.edu>
2011-05-12 15:22:05 -0400

    Basic tests for registration and search

--- twilltests/test_a.py
+++ twilltests/test_a.py
@@ -23,7 +23,38 @@ class TestASar(unittest.TestCase):
     def test_form_on_page_up(self):
         self.b.go(self.pg)
         html = self.b.result.get_page()
-        assert(html.find('form')>0)    
+        assert(html.find('form')>0)
+        
+    def test_registration(self):
+        ul = ["test_reg_user1", "test_reg_user2", "test_reg_user3"]
+        p = "abc123"
+        em = "noreplysavegame@gmail.com"
+        for i in range(len(ul)):
+            self.b.go(self.pg+"/registration")
+            fv (3, "username", ul[i])
+            fv (3, "password", p)
+            fv (3, "repassword", p)
+            fv (3, "email", em)
+            submit()
+            reset_browser()
+            self.b = get_browser()
+            self.b.go(self.pg)
+            fv (2, "username", ul[i])
+            fv (2, "password", p)
+            submit()
+            html = self.b.result.get_page()
+            assert(html.find("Invalid Username or Password: Please check your credentials.") == -1)
+        
+    def test_search(self):
+        sl = ["portal", "port", "akshai", "cry"]
+        rl = ["Portal 2", "Portal 2", "World of Warcraft", "Crysis 2"]
+        for i in range(len(sl)):
+            self.b.go(self.pg)
+            fv("search_form", "search", sl[i])
+            submit()
+            sleep()
+            html = self.b.result.get_page()
+            assert(html.find(rl[i]) > 0)
 	
     def tearDown(self):
         if (self.cur):
