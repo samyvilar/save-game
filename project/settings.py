2be467fb61140c74f4b05658e9dcf4f95b2acc2a
Townes Wang <townes.wang@hotmail.com>
2011-05-13 03:24:04 -0400

    Installation of django-ajax-selects-1.1.4 and the Autocomplete plugin for jQuery.

--- project/settings.py
+++ project/settings.py
@@ -142,7 +142,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'savegame'
+    'savegame',
+    'ajax_select'
 )
 
 # A sample logging configuration. The only tangible logging
