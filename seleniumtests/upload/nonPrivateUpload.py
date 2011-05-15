b0d4c5cc1fae94423f71e2ba4e7b28661c880ac7
Townes Wang <townes.wang@hotmail.com>
2011-05-14 22:44:15 -0400

    Completed tests for the uploading of saved data.

---
+++ seleniumtests/upload/nonPrivateUpload.py
@@ -0,0 +1,61 @@
+from selenium import selenium
+import unittest, time, re
+
+class nonPrivateUpload(unittest.TestCase):
+    def setUp(self):
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "http://127.0.0.1:8000/")
+        self.selenium.start()
+    
+    def test_non_private_upload(self):
+        sel = self.selenium
+        sel.open("/signOut/")
+        sel.click("logo_title")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_element_present("//form[@action='/signIn/']"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("username")
+        sel.type("username", "townes.wang")
+        sel.type("password", "test")
+        sel.click("//input[@value='Login']")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_text_present("UPLOAD"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Welcome, Townes Wang"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("link=UPLOAD")
+        sel.wait_for_page_to_load("30000")
+        sel.type("id_game", "22038")
+        sel.select("id_platform", "label=PC")
+        sel.type("id_file", "C:\\Users\\GLaDOS\\Desktop\\test.txt")
+        sel.type("id_title", "Non-Private Upload Test")
+        sel.type("id_description", "Non-Private Upload Test\n== Description ==")
+        sel.click("upload_submit")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_text_present("Your file has been successfully uploaded."))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("You will be redirected back to the upload page in 10 seconds."))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("//a[contains(text(),'If you do not wish to wait, please click here to return to the\n                    upload page.')]")
+        sel.wait_for_page_to_load("30000")
+        sel.click("logo_title")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_text_present("Non-Private Upload Test"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("WTF: Work Time Fun"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("link=Townes Wang")
+        sel.wait_for_page_to_load("30000")
+        try: self.failUnless(sel.is_text_present("Non-Private Upload Test"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("WTF: Work Time Fun"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Non-Private Upload Test == Description =="))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+    
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
